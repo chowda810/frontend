c9ba931f2fb2538458976c41842c760f92d5e2ea
chowda810 <chowda810@gmail.com>
2024-04-05 20:01:38 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import Provider from './Context/Provider';
+// pootstrap css
+import 'bootstrap/dist/css/bootstrap.min.css';
+//react quill css
+import 'react-quill/dist/quill.snow.css';
+// toast css 
+import 'react-toastify/dist/ReactToastify.css';
+// react-tooltip css
+import 'react-tooltip/dist/react-tooltip.css';
+// dataPicker css
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <BrowserRouter>
+    <Provider>
+      <App />
+      </Provider>
+      <ToastContainer />
+    </BrowserRouter>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
